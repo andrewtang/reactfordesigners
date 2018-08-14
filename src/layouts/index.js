f77403d841a15ff9c14c6b280fa4def1ac0008a8
Andrew Tang <andrewta@andrews-mbp.au.ibm.com>
2018-08-14 16:03:30 +1000

    minor updates

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-
 import Header from '../components/header'
+import Card from '../components/cards'
 import './index.css'
-
+import Section from '../components/section'
 
 const Layout = ({ children, data }) => (
   <div>
@@ -17,7 +17,37 @@ const Layout = ({ children, data }) => (
     />
    <Header />
    {children()}
-  </div>
+
+		<div className="CardGroup">
+			<Card 
+				title="DesignSystem"
+				text="10 sections"
+				image={require('../images/wallpaper.jpg')} />
+			<Card 
+				title="React for Designers"
+				text="11 sections"
+				image={require('../images/wallpaper2.jpg')} />
+			<Card 
+				title="Sound Design"
+				text="5 sections"
+				image={require('../images/wallpaper3.jpg')} />
+			<Card 
+				title="ARKit 2"
+				text="10 sections"
+				image={require('../images/wallpaper4.jpg')} />
+			<Card 
+				title="Augmented Reality"
+				text="12 sections"
+				image={require('../images/wallpaper.jpg')} />
+		</div>
+
+		<Section
+			image={require('../images/wallpaper2.jpg')}
+			logo={require('../images/logo-react.png')}
+			title="React for Designers"
+			text="Learn how to build a modern site using React and the most efficient libraries to get your site/product online. Get familiar with components, Grid CSS, animations, interactions, dynamic data with Contentful and deploying your site with Netlify." />
+
+</div>
 )
 
 Layout.propTypes = {
